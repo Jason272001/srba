5cdb6271a9a66ac2e638eb2beb45b51715e13427
Jason272001 <93046082+Jason272001@users.noreply.github.com>
2024-07-04 23:42:07 -0700

    asd

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,6 +3,7 @@ import logob from "../../logob.jpeg";
 import { AiOutlineMenuUnfold, AiOutlineClose } from "react-icons/ai";
 import { FaHandshake } from "react-icons/fa";
 import alogo from "../../logoA.mp4";
+
 const Nav = () => {
   const [showNav, setShowNav] = useState(false);
 
@@ -15,10 +16,16 @@ const Nav = () => {
       <div className="flex flex-col bg-black shadow-md">
         <div className="flex flex-row justify-between p-4 md:px-32 px-5 bg-black shadow-[0_3px_105px_rgba(0,0,0,0.3)]">
           <div className="flex flex-row items-center cursor-pointer px-0">
-            <video className="h-24 w-32 " loop muted autoPlay>
+            <video
+              className="h-24 w-32"
+              loop
+              muted
+              autoPlay
+              preload="auto"
+              playsInline
+            >
               <source src={alogo} type="video/mp4" />
             </video>
-            s
           </div>
           <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8 px-5">
             <a href="#" className="text-white hover:text-gray-400">
